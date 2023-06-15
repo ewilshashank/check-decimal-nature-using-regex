3ae83f0651a1fcc967f089a8da6f5a20427d712e
Code With Ewil <93051565+ewilshashank@users.noreply.github.com>
2023-06-15 13:58:35 +0530

    Update decimalExpansionKind.js

--- decimalExpansionKind.js
+++ decimalExpansionKind.js
@@ -1,20 +1,20 @@
-function findDecimalNature(numerator, denominator){
-    let quotient = numerator/denominator;
-    let decimalPart = quotient.toString().charAt.split('.');
+function findDecimalNature(numerator, denominator) {
+  let quotient = numerator / denominator;
+  let decimalPart = quotient.toString().split('.')[1]; 
 
-    if(!decimalPart){
-        return "terminating";
-    }else if(/^0+$/.test(decimalPart)){
-        return "terminating non recurring";
-    }else if(/^(\d+?)\1+$/.test(decimalPart)){
-        if(decimalPart === 1){
-            return "terminating Recurring";
-        }else {
-            return "non-terminating Recurring";
-        }
-    }else{
-        return "non-terminating non-recurring";
+  if (!decimalPart) {
+    return "terminating";
+  } else if (/^0+$/.test(decimalPart)) {
+    return "terminating non-recurring"; 
+  } else if (/^(\d+?)\1+$/.test(decimalPart)) {
+    if (parseInt(decimalPart) === 0) { 
+      return "terminating recurring"; 
+    } else {
+      return "non-terminating recurring"; 
     }
+  } else {
+    return "non-terminating non-recurring"; 
+  }
 }
 
 let numerator = 7;
